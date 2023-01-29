2eda60ff2dca758a76231c0481f5ef39d1d6fc54
Neil <neil.b.lasley@outlook.com>
2023-01-29 15:31:44 -0800

    lesson 2 challenge 1 calculator assignment.

---
+++ lesson02/Calculator.js
@@ -0,0 +1,23 @@
+let x = 4;
+let y = 8;
+let z = 12;
+let m = 24;
+
+if ((x + y) === z ) {
+  console.log('Here we go!');
+} else {
+  console.log('Oh no!');
+};
+
+while (x <= m) {
+  console.log('Thank you Mario! But our Princess is in another castle! She is with King Bowser in level ' + m);
+  x++;
+};
+
+for (let x = 0; x < z; x++) {
+  console.log('Wahoo!!');
+};
+
+if (((x + y) && (m - z)) <= 24) {
+  console.log('Oh yeah, Mario time!!');
+};
