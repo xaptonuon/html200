83f06b5d8af5c164c0a97b20fd6eceb942644f67
Neil <neil.b.lasley@outlook.com>
2023-02-20 19:01:19 -0800

    added lesson 3 assignment files.

---
+++ lesson03/lesson3-challenge2.js
@@ -0,0 +1,7 @@
+const sentence = "Homemade bread is much better than store bought!";
+
+const breadTruth = sentence.split(' ');
+
+console.log(sentence);
+
+console.log(breadTruth[2]);
---
+++ lesson03/lesson3-challenge3.js
@@ -0,0 +1,5 @@
+const liquidBread = ['Porter', 'Stout', 'Ale', 'Lager', 'Hefeweizen'];
+
+for (let i = 0; i < liquidBread.length; i++) {
+    console.log(i + ", " + liquidBread[i]);
+}
